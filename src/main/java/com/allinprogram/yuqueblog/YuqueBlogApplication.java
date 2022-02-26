e65209e6dc8e4c5becbd478741b77ad3dadb993d
AllinProgram <ufan0@foxmail.com>
2022-02-26 16:20:03 +0800

    Initial commit

---
+++ src/main/java/com/allinprogram/yuqueblog/YuqueBlogApplication.java
@@ -0,0 +1,13 @@
+package com.allinprogram.yuqueblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuqueBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuqueBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allinprogram/yuqueblog/YuqueBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allinprogram.yuqueblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuqueBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
