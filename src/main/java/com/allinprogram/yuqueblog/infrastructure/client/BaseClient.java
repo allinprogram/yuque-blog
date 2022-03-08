1409da4847c9b5a9e85c10e05dbbe029c01a7381
AllinProgram <ufan0@foxmail.com>
2022-03-09 00:33:36 +0800

    构建HTTP调用

--- src/main/java/com/allinprogram/yuqueblog/infrastructure/client/BaseClient.java
+++ src/main/java/com/allinprogram/yuqueblog/infrastructure/client/BaseClient.java
@@ -13,7 +13,7 @@ import static com.allinprogram.yuqueblog.common.Constant.YuQueApi.BASE;
         headers = {
                 "Content-Type: application/json",
                 "User-Agent: yuque-blog",
-                "X-Auth-Token: 9W90xI9QzhUY9UXGGJn4YRCPCzH3bKxcRuNxRn1I"
+                "X-Auth-Token: "
         }
 )
 public interface BaseClient {
