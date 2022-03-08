034ae7fbc7afe3cc9a49e673cbeb78e37ec1f589
AllinProgram <ufan0@foxmail.com>
2022-03-08 16:35:51 +0800

    部分框架不支持JDK17 + Spring boot3
    将开发环境降级为Spring Boot2.6.4

--- src/main/java/com/allinprogram/yuqueblog/YuqueBlogApplication.java
+++ src/main/java/com/allinprogram/yuqueblog/YuqueBlogApplication.java
@@ -1,15 +1,10 @@
 package com.allinprogram.yuqueblog;
 
-import com.allinprogram.yuqueblog.component.UserComponent;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class YuqueBlogApplication {
-
-	@Autowired
-	private static UserComponent userComponent;
 
 	public static void main(String[] args) {
 		SpringApplication.run(YuqueBlogApplication.class, args);
