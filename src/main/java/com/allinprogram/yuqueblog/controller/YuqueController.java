0d8f0162d72e59873fb7139d4b5fe8157247d150
AllinProgram <ufan0@foxmail.com>
2022-03-10 22:01:51 +0800

    增加web path

--- src/main/java/com/allinprogram/yuqueblog/controller/YuqueController.java
+++ src/main/java/com/allinprogram/yuqueblog/controller/YuqueController.java
@@ -3,6 +3,7 @@ package com.allinprogram.yuqueblog.controller;
 import com.allinprogram.yuqueblog.infrastructure.client.UserClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-03-08 13:39 星期二
  */
 @RestController
+@RequestMapping("api")
 public class YuqueController {
 
     @Autowired
     private UserClient userClient;
 
-    @GetMapping("")
+    @GetMapping("user")
     public String getUser() {
         return userClient.getUser();
     }
