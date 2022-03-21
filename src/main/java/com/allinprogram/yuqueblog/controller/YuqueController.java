769eef74dc4f8c8a5b183a6961762e33d1a029a5
AllinProgram <ufan0@foxmail.com>
2022-03-22 00:19:37 +0800

    rename Controller

--- src/main/java/com/allinprogram/yuqueblog/controller/YuqueController.java
+++ src/main/java/com/allinprogram/yuqueblog/controller/YuqueController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 语雀API封装
+ * 语雀API Controller
  *
  * @author AllinProgram
  * @since 2022-03-08 13:39 星期二
