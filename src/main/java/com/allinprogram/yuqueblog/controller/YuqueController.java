a4759958fccaebc646608ce054f9a33f94740d91
AllinProgram <ufan0@foxmail.com>
2022-03-29 01:16:45 +0800

    增加待办事项

--- src/main/java/com/allinprogram/yuqueblog/controller/YuqueController.java
+++ src/main/java/com/allinprogram/yuqueblog/controller/YuqueController.java
@@ -5,21 +5,19 @@ import com.allinprogram.yuqueblog.infrastructure.client.RepoClient;
 import com.allinprogram.yuqueblog.infrastructure.client.UserClient;
 import com.allinprogram.yuqueblog.infrastructure.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.wildfly.common.annotation.NotNull;
 
 import java.util.List;
 
 /**
  * 语雀API Controller
+ * TODO 可以加上注解校验器 by Hibernate
  *
  * @author AllinProgram
  * @since 2022-03-08 13:39 星期二
  */
-@Validated
 @RestController
 @RequestMapping("api")
 public class YuqueController {
@@ -36,7 +34,7 @@ public class YuqueController {
     }
 
     @GetMapping("repos")
-    public List<RepoDTO> getRepos(@NotNull Integer userId) {
+    public List<RepoDTO> getRepos(Integer userId) {
         return YuqueRespParseUtil.parseList(userClient.getRepos(userId), RepoDTO.class);
 
     }
