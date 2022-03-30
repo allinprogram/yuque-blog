98d498c6d0b0594225d3ec96d22389a759eb8351
AllinProgram <ufan0@foxmail.com>
2022-03-30 23:51:02 +0800

    删除注解校验

--- src/main/java/com/allinprogram/yuqueblog/controller/YuqueController.java
+++ src/main/java/com/allinprogram/yuqueblog/controller/YuqueController.java
@@ -4,7 +4,6 @@ import com.allinprogram.yuqueblog.common.YuqueRespParseUtil;
 import com.allinprogram.yuqueblog.infrastructure.client.YuqueApiClient;
 import com.allinprogram.yuqueblog.infrastructure.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +16,6 @@ import java.util.List;
  * @author AllinProgram
  * @since 2022-03-08 13:39 星期二
  */
-@Validated
 @RestController
 @RequestMapping("api")
 public class YuqueController {
