008b896a37bc61c04fd48fc4a654731e54676a9b
AllinProgram <ufan0@foxmail.com>
2022-03-31 16:05:51 +0800

    丰富页面

--- src/main/java/com/allinprogram/yuqueblog/controller/WebController.java
+++ src/main/java/com/allinprogram/yuqueblog/controller/WebController.java
@@ -1,7 +1,13 @@
 package com.allinprogram.yuqueblog.controller;
 
+import com.allinprogram.yuqueblog.infrastructure.dto.DocDTO;
+import com.allinprogram.yuqueblog.infrastructure.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * @author AllinProgram
@@ -10,8 +16,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping
-    public String index() {
+    @Autowired
+    private YuqueController yuqueController;
+
+    @GetMapping({"", "index"})
+    public String index(Model model) {
+        UserDTO user = yuqueController.getUser();
+        model.addAttribute("user", user);
+
+        List<DocDTO> docs = yuqueController.getDocs();
+        model.addAttribute("docs", docs);
         return "index";
+    }
+
+    @GetMapping("about")
+    public String about(Model model) {
+        model.addAttribute("user", yuqueController.getUser());
+        return "about";
     }
 }
